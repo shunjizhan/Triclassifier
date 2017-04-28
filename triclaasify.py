a1c3608b32ae5de00c76b3b52dbdb129778cedee
shunji zhan <shunji.zhan@procore.com>
2017-04-28 14:18:55 -0700

    finished trainign

--- triclaasify.py
+++ triclaasify.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 def printList(list):
   for p in list: print p
@@ -6,6 +7,14 @@ def printList(list):
 def readFile(filename):
   lines = [line.rstrip('\n') for line in open(filename)]
   return lines
+
+def distance(a, b):
+  dimension = len(a)
+  theSum = 0
+  for i in range (0, i):
+    theSum += abs(a[i]**2 - b[i]**2)
+
+  return sqrt(theSum)
 
 def findCentroids(data, dimension):
   theSum = []
@@ -24,9 +33,22 @@ def findCentroids(data, dimension):
 
   return centroid
 
-#~~~~~~~~~~ Main ~~~~~~~~~~#
-trainingData = readFile(sys.argv[1]);
+def predict(point, centroid_A, centroid_B, centroid_C):
+  distance_A = distance(centroid_A, point)
+  distance_B = distance(centroid_B, point)
+  distance_A = distance(centroid_C, point)
 
+  if (distance_A < distance_B and distance_A < distance_C):
+    return 'A'
+  elif (distance_B < distance_C):
+    return 'B'
+  else:
+    return 'C'
+
+#~~~~~~~~~~ Main ~~~~~~~~~~#
+
+########## training process ##########
+trainingData = readFile(sys.argv[1])
 info = trainingData[0].split(' ')
 trainingData = trainingData[1 :]
 
@@ -45,7 +67,19 @@ centroid_A = findCentroids(data_A, dimension)
 centroid_B = findCentroids(data_B, dimension)
 centroid_C = findCentroids(data_C, dimension)
 
-print centroid_A, centroid_B, centroid_C
+########## testing process ##########
+testingData = readFile(sys.argv[2])
+info_test = testingData[0].split(' ')
+testingData = testingData[1 :]
+
+dimension = int(info_test[0])
+count_A_test = int(info_test[1])
+count_B_test = int(info_test[2])
+count_C_test = int(info_test[3])
+
+
+
+# print centroid_A, centroid_B, centroid_C
 
 # printList(data_A)
 # printList(data_B)
